9f03629c99b48b226c90bffd66eca3d4b9ead363
= <vinurakav@gmail.com>
2022-08-17 17:07:31 +0530

    Add polynomial rolling hash function

--- hashing.cpp
+++ hashing.cpp
@@ -1,14 +1,24 @@
 #include "hashing.hpp"
+#include <cmath>
 
 int hash_string(string s) {
     // implement hashing function here
-    int prime = 67; 
+    int m = 67; 
+    int p = 37;
 
-    int sum = 0;
+    int val = 1;
+
+    long long int sum = 0;
     for (int i = 0; i < s.length(); i++) {
-        sum += (int)s[i];
+        int character = s[i];
+        int val = 1;
+        for (int j = 0; j < i; j++) {
+            val *= p;
+            val %= m;
+        }
+        sum += (val * character%m) % m;        
     }
     
-    return sum % prime;
+    return sum % m;
 
 }
